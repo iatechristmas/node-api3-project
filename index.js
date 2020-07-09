2567a073ed05552b864196e249d60559019cc421
Big Chungus <iatechristmas@gmail.com>
2020-07-08 17:59:49 -0700

    finished mvp

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require("./server");
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+  console.log(`**** listening on port ${PORT}`);
+});
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
+const morgan = require("morgan");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.use(express.json()); // Built in middleware, no need to npm install it
+server.use("/api/posts", postRouter);
+server.use("/api/users", morgan("dev"), userRouter);
+server.use(morgan("dev"));
+
+server.get("/", (req, res) => {
+  res.status(200).json({ enviroment: process.env.NODE_ENV });
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const date = new Date().toTimeString();
+  console.log(`\n${req.method} ${req.url} ${res.statusCode} [${date}]\n`);
+  next();
+}
 
 module.exports = server;
